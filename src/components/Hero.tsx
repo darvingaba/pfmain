2097b3fb7d78bdf19d31e71d970f441a457e73f2
darvingaba <69685707+darvingaba@users.noreply.github.com>
2024-02-16 22:22:05 +0000

    /

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,12 +16,12 @@ function Hero() {
           </p>
 
           <a
-            href="#about"
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            href="#about" 
+            className="group inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
           >
             Get started
             <svg
-              className="w-5 h-5 ml-2 -mr-1 hover:rotate-45 transition-transform"
+              className="w-5 h-5 ml-2 -mr-1 group-hover:rotate-90 transition-transform"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
